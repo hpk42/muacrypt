99e421b617936b322cc6bfe228563838962eeeaa
juga0 <juga@riseup.net>
2017-11-11 15:51:15 +0000

    Add logging configuration to file

---
+++ core/autocrypt/conflog.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# vim:ts=4:sw=4:expandtab
+# Copyright 2016, 2017 juga (juga at riseup dot net), MIT license.
+"""Logging configuration."""
+import logging
+import sys
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s'
+                      ' %(filename)s:%(lineno)s -'
+                      ' %(funcName)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': "%(message)s",
+        },
+        'sys': {
+            'format': "%(module)s[%(process)s]: "
+                      "%(message)s"
+        }
+    },
+    'handlers': {
+        'stdout': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'formatter': 'verbose',
+            'level': 'DEBUG',
+        },
+        'syslog': {
+            'class': 'logging.handlers.SysLogHandler',
+            'address': '/dev/log',
+            'formatter': 'sys',
+            'level': 'INFO',
+        },
+    },
+    'loggers': {
+        'autocrypt': {
+            'handlers': ['syslog', 'stdout'],
+            'level': logging.INFO,
+            'propagate': False
+        },
+    }
+}
