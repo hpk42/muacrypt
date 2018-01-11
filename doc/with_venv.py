9f430eff76b5a30006a0a0655658549511064510
holger krekel <holger@merlinux.eu>
2018-01-11 14:38:17 +0100

    fix doc build

--- doc/with_venv.py
+++ doc/with_venv.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     os.environ["PATH"] = bindir + os.pathsep + os.environ["PATH"]
     if not os.path.exists(venvdir):
         subprocess.check_call(["virtualenv", "-p", sys.executable, venvdir])
-        subprocess.check_call(["pip", "install", "-e", "../core"])
+        subprocess.check_call(["pip", "install", "-e", "../"])
 
     # poor people's virtualenv activate
     sys.exit(subprocess.call(sys.argv[2:]))
