9b4177ed60a465057b00dcdefad3f544cf2d989d
holger krekel <holger@merlinux.eu>
2018-01-11 14:45:31 +0100

    remove syspath insertion which is not needed with readthedocs because you can switch on "execute setup.py install" in advanced settings

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,6 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join("..", "core")))
 from muacrypt import __version__ as release
 version = ".".join(release.split(".")[:2])
 
