aa07c919ebc02e0a248be810809ca15abd2c1127
Azul <azul@riseup.net>
2018-01-23 15:23:19 +0100

    travis: use custom swdb.lst
    Verifying the downloaded one with gpgv seems to fail.
    No idea why... or rather it cannot find the public keys
    even if i add them to the default gpg1 and gpg2 keyrings
    plus ~/.gnupg/trustedkeys.gpg
    
    I doubt this will work on its own because i think the
    existance of swdb.lst on my local machine is an artifact
    of previous attempts to build. Trying this to figure that
    out.

--- util/custom-gpg.sh
+++ util/custom-gpg.sh
@@ -6,6 +6,6 @@ gpg2 --version | grep "gpg (GnuPG) $GPG_VERSION" && exit 0
 wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-$GPG_VERSION.tar.bz2
 bzcat gnupg-$GPG_VERSION.tar.bz2 | tar xf -
 cd gnupg-$GPG_VERSION
-make -f build-aux/speedo.mk native INSTALL_PREFIX="$HOME"
+make -f build-aux/speedo.mk native INSTALL_PREFIX="$HOME" CUSTOM_SWDB=1
 
 gpg2 --version | grep "gpg (GnuPG) $GPG_VERSION"
