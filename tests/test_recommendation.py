f57eb5ad63b11ba15144c4c5d122249a3f394d5f
Azul <azul@riseup.net>
2018-01-27 16:38:59 +0100

    test: recommendation after receiving ac header

--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
 # vim:ts=4:sw=4:expandtab
 
+from muacrypt import mime
 from muacrypt.recommendation import Recommendation
+
+
+def send_ac_mail(sender, recipient):
+    mail = mime.gen_mail_msg(
+        From=sender.addr, To=[recipient.addr],
+        Autocrypt=sender.make_ac_header(recipient.addr),
+        payload=None, charset=None, Date=None,
+    )
+    recipient.process_incoming(mail)
 
 
 class TestRecommendation:
 
-    def test_empty_peer_state(self, account_maker):
+    def test_initial_mail(self, account_maker):
         sender, recipient = account_maker(), account_maker()
         peerstate = sender.get_peerstate(recipient.addr)
         rec = Recommendation({recipient.addr: peerstate})
         assert rec.target_keys()[recipient.addr] is None
         assert rec.ui_recommendation() == 'disable'
+
+    def test_reply_to_ac_mail(self, account_maker):
+        sender, recipient = account_maker(), account_maker()
+        send_ac_mail(sender, recipient)
+        peerstate = recipient.get_peerstate(sender.addr)
+        rec = Recommendation({sender.addr: peerstate})
+        assert rec.target_keys()[sender.addr]
+        assert rec.ui_recommendation() == 'available'
