299cebe009f4b3b1ad999c6fe1ec8345c44d57b5
Azul <azul@riseup.net>
2018-01-27 16:52:36 +0100

    test: recommendation after receiving no ac header

--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -13,20 +13,35 @@ def send_ac_mail(sender, recipient):
     )
     recipient.process_incoming(mail)
 
+def send_no_ac_mail(sender, recipient):
+    mail = mime.gen_mail_msg(
+        From=sender.addr, To=[recipient.addr],
+        Autocrypt=None, payload=None, charset=None, Date=None,
+    )
+    recipient.process_incoming(mail)
+
 
 class TestRecommendation:
 
     def test_initial_mail(self, account_maker):
-        sender, recipient = account_maker(), account_maker()
-        peerstate = sender.get_peerstate(recipient.addr)
-        rec = Recommendation({recipient.addr: peerstate})
-        assert rec.target_keys()[recipient.addr] is None
+        composer, peer = account_maker(), account_maker()
+        peerstate = composer.get_peerstate(peer.addr)
+        rec = Recommendation({peer.addr: peerstate})
+        assert rec.target_keys()[peer.addr] is None
         assert rec.ui_recommendation() == 'disable'
 
     def test_reply_to_ac_mail(self, account_maker):
-        sender, recipient = account_maker(), account_maker()
-        send_ac_mail(sender, recipient)
-        peerstate = recipient.get_peerstate(sender.addr)
-        rec = Recommendation({sender.addr: peerstate})
-        assert rec.target_keys()[sender.addr]
+        composer, peer = account_maker(), account_maker()
+        send_ac_mail(peer, composer)
+        peerstate = composer.get_peerstate(peer.addr)
+        rec = Recommendation({peer.addr: peerstate})
+        assert rec.target_keys()[peer.addr]
         assert rec.ui_recommendation() == 'available'
+
+    def test_reply_to_no_ac_mail(self, account_maker):
+        composer, peer = account_maker(), account_maker()
+        send_no_ac_mail(peer, composer)
+        peerstate = composer.get_peerstate(peer.addr)
+        rec = Recommendation({peer.addr: peerstate})
+        assert rec.target_keys()[peer.addr] is None
+        assert rec.ui_recommendation() == 'disable'
